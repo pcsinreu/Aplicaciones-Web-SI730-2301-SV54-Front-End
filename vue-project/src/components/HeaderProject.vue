966d7035a4b4d359a7aa968a2bece0b0c5920c66
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-04-03 22:34:03 -0500

    feat(component) add new component

--- vue-project/src/components/HeaderProject.vue
+++ vue-project/src/components/HeaderProject.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>My component {{name}}</div>
+    <div class="containerGrid">
+      <div class="my-class">My component {{name}}</div>
+        <div>
+            <div class="my-class">My component part 2  {{name}}</div>
+          <div class="my-class">My component part 2  {{name}}</div>
+        </div>
+      <div class="my-class">My component part 3 {{name}}</div>
+    </div>
 </template>
 
 <script>
@@ -11,4 +18,21 @@ export default {
 
 <style scoped>
 
+.my-class{
+    background-color: red;
+    margin-top: 10px;
+}
+
+.container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+
+.containerGrid {
+    display: Grid;
+    grid-template-columns:1fr 1fr;
+    grid-gap:10px;
+}
 </style>
