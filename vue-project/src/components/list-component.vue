bcb8a02d112ce46217c3c58da97584079e3e493f
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-04-17 22:24:49 -0500

    feat(Package) add vue router

--- vue-project/src/components/list-component.vue
+++ vue-project/src/components/list-component.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="grid">
-        <div v-for="category in categories" >
+        <div v-for="category in users" >
             <myComponent :name=category.name class="col" />
         </div>
     </div>
@@ -17,6 +17,7 @@ export default {
     data() {
         return{
             categories :[],
+            users :null,
             userService : new UserServices()
         }
     },
@@ -24,15 +25,13 @@ export default {
         getAllUser: function (){
             this.userService.getUser().then(function (response) {
                 console.log('response',response.data)
-                this.categories = response.data
+                this.users = response.data
         });
         }
     },
     mounted() {
         //invoco API
         this.getAllUser();
-
-
         //single responsability
         //this.categories = [{name:'nombre 1'},{name:'nombre 2'},{name:'nombre 3'},{name:'nombre 4'}]
     }
--- vue-project/src/services/user-api.services.js
+++ vue-project/src/services/user-api.services.js
@@ -1,27 +1,37 @@
 import axios from 'axios'
 
+const http = axios.create({
+    baseURL:'https://jsonplaceholder.typicode.com/'
+})
+
+
 export class UserServices{
 
     getUser=()=>{
-
-       return axios({
-            method: 'get',
-            url: 'https://jsonplaceholder.typicode.com/users'
-        })
-
-
+       return http.get('users')
     };
-    getUserById= (id) =>{
-
+    getUserById= (id) => {
+        return http.get('users' +id);
     };
 
     postUser =(body)=>{
 
+        return http.postUser('users',{ body});
+
     };
     putUser =(body,id)=>{
+        return axios({
+            method: 'post',
+            url: 'users' +id,
+            data:body
+        })
 
     }
     deletUser =(id)=>{
+        return axios({
+            method: 'delete',
+            url: 'users' +id
+        })
 
     }
 
