174c0e563d878d274ce3a229e2ba0d9427f1ccba
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-04-24 20:03:16 -0600

    feat(Update)

--- vue-project/src/router.js
+++ vue-project/src/router.js
@@ -3,14 +3,15 @@ import FormData from "@/components/form-data.component.vue";
 import ListComponent from "@/components/list-component.vue";
 import NotFound from "@/components/NotFound.vue";
 import {createRouter,createWebHashHistory}    from 'vue-router'
-import UserForm from "@/components/user-form.vue";
-import ListUser from "@/components/list-user.vue";
+import UserEditForm from "@/components/user-edit-form.vue";
+import ListUser from "@/components/user-list.vue";
+import UserCreateForm from "@/components/user-create-form.vue";
 
 const routes = [
     {path:'/',component:Home },
     {path:'/AddTutorial/:id',component:FormData },
-    {path:'/AddUser',component:UserForm },
-    {path:'/User/:id',component:UserForm,name: 'user', },
+    {path:'/AddUser',component:UserCreateForm },
+    {path:'/User/:id',component:UserEditForm,name: 'user', },
     {path:'/ListComponent',component:ListComponent },
     {path:'/ListUser',component:ListUser },
     {path:'/:catchAll(.*)', component: NotFound}
