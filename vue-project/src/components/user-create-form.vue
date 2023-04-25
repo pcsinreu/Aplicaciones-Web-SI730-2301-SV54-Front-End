d3b096fdface9a369c71b42c011feea032716a66
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-04-24 20:05:31 -0600

    feat(Update)

--- vue-project/src/components/user-create-form.vue
+++ vue-project/src/components/user-create-form.vue
@@ -1,13 +1,47 @@
 <template>
+    <div class="my-form">
+        <div>Create new User</div>
+        <label for="name2">Name :</label>
+        <pv-inputText v-model="name2" id="name2"></pv-inputText>
+        <label for="username">Username :</label>
+        <pv-inputText v-model="username" id="username"></pv-inputText>
+        <pv-button label="Save" @click="saveuser()"></pv-button>}
+    </div>
 
 </template>
 
 <script>
+import {UserServices} from "@/services/user-api.services";
 export default {
-    name: "new-user-form"
+    name: "new-user-form",
+    methods:{
+
+        saveuser(){
+
+            const body ={ 'name':this.name2 ,'username': this.username}
+            //const body ={ this.name ,this.username}
+
+            this.userService.postUser(body).then((response)=>{
+                if( response.status === 201){
+                    alert('Created')
+                    this.$router.push('/')
+                }
+                else{
+                    alert('Error creating user')
+                }
+
+            })
+        }
+    }
 }
 </script>
 
 <style scoped>
+
+.my-form{
+    display:flex;
+    flex-direction: column;
+    background-color: beige;
+}
 
 </style>
--- vue-project/src/components/user-edit-form.vue
+++ vue-project/src/components/user-edit-form.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="my-form">
+        <div>Edit User</div>
         <label for="name2">Name :</label>
          <pv-inputText v-model="name2" id="name2"></pv-inputText>
         <label for="username">Username :</label>
         <pv-inputText v-model="username" id="username"></pv-inputText>
-        <pv-button label="Save" @click="saveuser()"></pv-button>}
+        <pv-button label="Save" @click="editUser()"></pv-button>}
     </div>
 </template>
 
@@ -22,21 +23,10 @@ export default {
         }
     },
     methods:{
-        saveuser(){
+        editUser(){
 
             const body ={ 'name':this.name2 ,'username': this.username}
-            //const body ={ this.name ,this.username}
 
-              this.userService.postUser(body).then((response)=>{
-                  if( response.status === 201){
-                        alert('Created')
-                       this.$router.push('/')
-                  }
-                  else{
-                      alert('Error creating user')
-                  }
-
-              })
         }
     },
     mounted() {
